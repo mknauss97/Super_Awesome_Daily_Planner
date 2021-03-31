eb62c46359d8a611073b4c4f22094d2c5f2da4da
Mknauss <mknauss97@gmail.com>
2021-03-30 21:57:57 -0400

    finished code with updated readme file

---
+++ assets/README.md
@@ -0,0 +1,10 @@
+# Super_Awesome_Daily_Planner
+The planner shows current day with timeblocks for each hour of the work day
+when the time is in the past the block will turn grey
+when the hour is the current time the block with turn red
+future hours will turn green
+when you type in an event and save it, it will be saved to local storage so it will stay even after you refresh the page
+
+link to deployed application src=https://mknauss97.github.io/Super_Awesome_Daily_Planner/
+
+![screenshot](assets/imgs/Capture.JPG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ function changeColor() {
 
     time.each(function() {
         var hour = parseInt($(this).attr('id'))
-
         if(hour === currentTime) {
             $(this).children('.schedule-block').attr('class', 'current col-sm-10 schedule-block' )
         } else if(currentTime > hour) {
@@ -17,6 +16,21 @@ function changeColor() {
         }else {
             $(this).children('.schedule-block').attr('class', 'future col-sm-10 schedule-block')
         }
+    }) 
+}changeColor();
+
+function createPlans() {
+    $(".row").each(function () {
+        var eventID = $(this).attr("id");
+        var plans = localStorage.getItem(eventID);
+        if(plans) {
+            $(this).children(".schedule-block").val(plans)
+        }
     })
-}
-changeColor();
+}createPlans()
+
+$('.save-button').on("click", function () {
+    var currentHour = $(this).parent().attr("id");
+    var plans = $(this).siblings(".schedule-block").val();
+    localStorage.setItem(currentHour, plans);
+})
